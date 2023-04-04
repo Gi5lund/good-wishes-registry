c849e46d4e3806b9efbb005a54c0e0fb80d0a77c
Gi5lund <mortengislund@gmail.com>
2023-04-04 21:19:11 +0200

    init

---
+++ src/main/java/dk/kea/dat22b/goodwishesregistry/GoodWishesRegistryApplication.java
@@ -0,0 +1,15 @@
+package dk.kea.dat22b.goodwishesregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodWishesRegistryApplication
+	{
+
+		public static void main(String[] args)
+			{
+				SpringApplication.run(GoodWishesRegistryApplication.class, args);
+			}
+
+	}
---
+++ src/test/java/dk/kea/dat22b/goodwishesregistry/GoodWishesRegistryApplicationTests.java
@@ -0,0 +1,15 @@
+package dk.kea.dat22b.goodwishesregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodWishesRegistryApplicationTests
+	{
+
+		@Test
+		void contextLoads()
+			{
+			}
+
+	}
