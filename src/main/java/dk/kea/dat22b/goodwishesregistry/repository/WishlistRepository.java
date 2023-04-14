670cadbc3651192c228b57cf99dc33fa89783432
Lotte Eliasson <lottefacereliasson@hotmail.com>
2023-04-14 10:11:14 +0200

    copy

--- src/main/java/dk/kea/dat22b/goodwishesregistry/repository/WishlistRepository.java
+++ src/main/java/dk/kea/dat22b/goodwishesregistry/repository/WishlistRepository.java
@@ -111,106 +111,7 @@ public class WishlistRepository
 				e.printStackTrace();
 			}
 		}
-		public void addWish(WishListItems wishListItems) {
 
-			try {
-				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
-				final String SQLcreatewish = "INSERT INTO wishlist.wish_list_items(wish_list_id, item_line_id, item_name, item_QTY, item_description, item_URL, item_price) VALUES(?,?,?,?,?,?,?)";
-				PreparedStatement preparedStatement = connection.prepareStatement(SQLcreatewish);
-
-				preparedStatement.setInt(1, wishListItems.getWishListId());
-				preparedStatement.setInt(2, wishListItems.getItemLineId());
-				preparedStatement.setString(3, wishListItems.getItemName());
-				preparedStatement.setInt(4, wishListItems.getItemQTY());
-				preparedStatement.setString(5, wishListItems.getItemDescription());
-				preparedStatement.setString(6, wishListItems.getItemURL());
-				preparedStatement.setDouble(7, wishListItems.getItemPrice());
-
-				preparedStatement.executeUpdate();
-
-			} catch (SQLException e) {
-				System.out.println("Could not create new wish");
-				e.printStackTrace();
-			}
-		}
-		public void updateWish(WishListItems wishListItems){
-			final String UPDATE_QUERY = "UPDATE wishlist.wish_list_items SET item_name=?, item_QTY=?, item_description=?, item_URL=?, item_price=? WHERE item_line_id=?";
-
-			try{
-				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
-				PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
-				String itemName = wishListItems.getItemName();
-				int itemQTY = wishListItems.getItemQTY();
-				String itemDescription = wishListItems.getItemDescription();
-				String itemURL = wishListItems.getItemURL();
-				double itemPrice = wishListItems.getItemPrice();
-
-				preparedStatement.setString(1, itemName);
-				preparedStatement.setInt(2, itemQTY);
-				preparedStatement.setString(3,itemDescription);
-				preparedStatement.setString(4, itemURL);
-				preparedStatement.setDouble(5,itemPrice);
-
-				preparedStatement.executeUpdate();
-
-			}catch(SQLException e){
-				System.out.println("Could not update wish");
-				e.printStackTrace();
-			}
-		}
-		public WishListItems findWishById(int itemLineId){
-			final String FIND_QUERY = "SELECT * FROM wishlist.wish_list_items WHERE item_line_id = ?";
-
-			WishListItems wishListItems = new WishListItems();
-			wishListItems.setItemLineId(itemLineId);
-
-			try{
-
-				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
-				PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
-				preparedStatement.setInt(1, itemLineId);
-				ResultSet resultSet = preparedStatement.executeQuery();
-
-				resultSet.next();
-				String itemName = resultSet.getString(3);
-				int itemQTY = resultSet.getInt(4);
-				String itemDescription = resultSet.getString(5);
-				String itemURL = resultSet.getString(6);
-				double itemPrice = resultSet.getDouble(7);
-
-				wishListItems.setItemName(itemName);
-				wishListItems.setItemQTY(itemQTY);
-				wishListItems.setItemDescription(itemDescription);
-				wishListItems.setItemURL(itemURL);
-				wishListItems.setItemPrice(itemPrice);
-
-			}catch (SQLException e){
-				System.out.println("Could not find item");
-				e.printStackTrace();
-			}
-			return wishListItems;
-		}
-		public void deleteById(int itemLineId){
-			//SQL-query
-			final String DELETE_QUERY = "DELETE FROM wishlist.wish_list_items WHERE item_line_id =?";
-
-			try{
-				//Connect til DB
-				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
-
-				//Create statement
-				PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
-
-				//Set parameter
-				preparedStatement.setInt(1, itemLineId);
-
-				//Execute statement
-				preparedStatement.executeUpdate();
-
-			}catch (SQLException e){
-				System.out.println("Could not delete item");
-			}
-		}
 		public List<WishList> getWishListByUserId(int userId) {
 			List<WishList> wishListe = new ArrayList<>();
 			try {
@@ -314,6 +215,107 @@ public class WishlistRepository
 			catch(SQLException e) {
 				System.out.println("Could not delete wishlist");
 				e.printStackTrace();
+			}
+		}
+
+		public void addWish(WishListItems wishListItems) {
+
+			try {
+				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+				final String SQLcreatewish = "INSERT INTO wishlist.wish_list_items(wish_list_id, item_line_id, item_name, item_QTY, item_description, item_URL, item_price) VALUES(?,?,?,?,?,?,?)";
+				PreparedStatement preparedStatement = connection.prepareStatement(SQLcreatewish);
+
+				preparedStatement.setInt(1, wishListItems.getWishListId());
+				preparedStatement.setInt(2, wishListItems.getItemLineId());
+				preparedStatement.setString(3, wishListItems.getItemName());
+				preparedStatement.setInt(4, wishListItems.getItemQTY());
+				preparedStatement.setString(5, wishListItems.getItemDescription());
+				preparedStatement.setString(6, wishListItems.getItemURL());
+				preparedStatement.setDouble(7, wishListItems.getItemPrice());
+
+				preparedStatement.executeUpdate();
+
+			} catch (SQLException e) {
+				System.out.println("Could not create new wish");
+				e.printStackTrace();
+			}
+		}
+		public void updateWish(WishListItems wishListItems){
+			final String UPDATE_QUERY = "UPDATE wishlist.wish_list_items SET item_name=?, item_QTY=?, item_description=?, item_URL=?, item_price=? WHERE item_line_id=?";
+
+			try{
+				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+				PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+				String itemName = wishListItems.getItemName();
+				int itemQTY = wishListItems.getItemQTY();
+				String itemDescription = wishListItems.getItemDescription();
+				String itemURL = wishListItems.getItemURL();
+				double itemPrice = wishListItems.getItemPrice();
+
+				preparedStatement.setString(1, itemName);
+				preparedStatement.setInt(2, itemQTY);
+				preparedStatement.setString(3,itemDescription);
+				preparedStatement.setString(4, itemURL);
+				preparedStatement.setDouble(5,itemPrice);
+
+				preparedStatement.executeUpdate();
+
+			}catch(SQLException e){
+				System.out.println("Could not update wish");
+				e.printStackTrace();
+			}
+		}
+		public WishListItems findWishById(int itemLineId){
+			final String FIND_QUERY = "SELECT * FROM wishlist.wish_list_items WHERE item_line_id = ?";
+
+			WishListItems wishListItems = new WishListItems();
+			wishListItems.setItemLineId(itemLineId);
+
+			try{
+
+				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+				PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
+				preparedStatement.setInt(1, itemLineId);
+				ResultSet resultSet = preparedStatement.executeQuery();
+
+				resultSet.next();
+				String itemName = resultSet.getString(3);
+				int itemQTY = resultSet.getInt(4);
+				String itemDescription = resultSet.getString(5);
+				String itemURL = resultSet.getString(6);
+				double itemPrice = resultSet.getDouble(7);
+
+				wishListItems.setItemName(itemName);
+				wishListItems.setItemQTY(itemQTY);
+				wishListItems.setItemDescription(itemDescription);
+				wishListItems.setItemURL(itemURL);
+				wishListItems.setItemPrice(itemPrice);
+
+			}catch (SQLException e){
+				System.out.println("Could not find item");
+				e.printStackTrace();
+			}
+			return wishListItems;
+		}
+		public void deleteById(int itemLineId){
+			//SQL-query
+			final String DELETE_QUERY = "DELETE FROM wishlist.wish_list_items WHERE item_line_id =?";
+
+			try{
+				//Connect til DB
+				Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+
+				//Create statement
+				PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+
+				//Set parameter
+				preparedStatement.setInt(1, itemLineId);
+
+				//Execute statement
+				preparedStatement.executeUpdate();
+
+			}catch (SQLException e){
+				System.out.println("Could not delete item");
 			}
 		}
 	}
